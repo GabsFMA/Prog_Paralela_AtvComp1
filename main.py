287fb041be98c4632642efd6f081c83c595f537e
Gabriel Francisco <105892017+GabsFMA@users.noreply.github.com>
2025-03-13 11:46:59 -0300

    feat: created function multi_thread_5_v1
    function to use 5 threads to read the input file, separate the numbers and put then in the output file

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def listar_primos():
             if primo:
                 lista_primos.append(numero)
 
-    print(f'Lista de primos: {lista_primos}')
+    #print(f'Lista de primos: {lista_primos}')
     
     with open ('output/Saida_01.txt', 'w') as saida:
         for numero in lista_primos:
@@ -43,30 +43,70 @@ def solo_thread():
     thread.join()
     tempo_final = time.time()
     duracao = tempo_final - tempo_inicial
-    print(f'Arquivo lido com sucesso, sua duração em segundos foi de:{round(duracao,2)}')
+    print(f'Processo concluído com sucessso, usando 1 Thread sua duração em segundos foi de:{round(duracao,2)}')
 
-solo_thread()
+#solo_thread()
 
 #----------------------------------------------------------
 
 #Fazendo uso de cinco threads
 
-def separar_primos(numeros, resultado):
-    lista_primo = []
+def listar_primos_5_v1(numeros, resultado, lock):
+    lista_primos = []
 
     for numero in numeros:
         if numero < 2:
             continue
-            
-        Primo = True
+
+        primo = True  
 
         for i in range(2, int(numero ** 0.5) + 1):
             if numero % i == 0:
-                Primo = False
+                primo = False
                 break
-            if Primo: 
-                lista_primo.append(numero)
-    resultado.extend(lista_primo)
-        
 
-        
+        if primo:
+            lista_primos.append(numero)
+
+   
+    with lock:
+        resultado.extend(lista_primos)
+
+def multi_thread_5_v1():
+    qnt_threads = 5
+    tempo_inicial = time.time()
+
+    with open('Entrada01.txt', 'r') as arquivo:
+        numeros = [int(linha.strip()) for linha in arquivo]
+
+    qnt_por_thread = len(numeros) // qnt_threads
+
+    partes = [numeros[i * qnt_por_thread: (i + 1) * qnt_por_thread] for i in range(qnt_threads)]
+
+    partes[-1].extend(numeros[qnt_threads * qnt_por_thread:])  
+
+    lista_primos_compartilhada = []
+    lock = threading.Lock()  
+    lista_threads = []
+
+    for i in range(qnt_threads):
+        thread = threading.Thread(target=listar_primos_5_v1, args=(partes[i], lista_primos_compartilhada, lock))
+        lista_threads.append(thread)
+        thread.start()
+
+    for thread in lista_threads:
+        thread.join()
+
+    with open('output/Saida_05_v1.txt', 'w') as saida:
+        for primo in sorted(lista_primos_compartilhada):
+            saida.write(f"{primo}\n")
+
+    tempo_final = time.time()
+    duracao = tempo_final - tempo_inicial
+    print(f'Processo concluído com sucessso, usando 5 Threads sua duração em segundos foi de:{round(duracao,2)}')
+
+#multi_thread_5_v1()
+
+
+
+
